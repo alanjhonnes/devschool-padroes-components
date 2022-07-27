75d2b44a015024ad89d2cfc9d31335038fcdd5db
alan.sa <alan.sa@mjv.com.br>
2022-07-26 23:58:17 -0300

    ajuste dynamic component

--- src/app/shared/components/dynamic/dynamic.component.ts
+++ src/app/shared/components/dynamic/dynamic.component.ts
@@ -34,8 +34,8 @@ export class DynamicComponent {
     const instance = componentRef.instance;
     instance.open = Math.random() >= 0.5;
     instance.count = Math.floor(Math.random() * 1000);
-    // this.viewContainerRef.insert(componentRef.hostView);
-    const compRef2 = this.container.createComponent(factory);
+    this.viewContainerRef.insert(componentRef.hostView);
+    // const compRef2 = this.container.createComponent(factory);
     // this.viewContainerRef.insert(componentRef.hostView);
     // this.viewContainerRef.createComponent(factory, undefined, this.injector);
   }
